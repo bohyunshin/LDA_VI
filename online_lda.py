43f6f71b3fe2ef6d90f441d35a7486a490dc83c9
shinbo <shinbo0613@gmail.com>
2020-08-01 23:39:58 +0900

    균등하게 수렴하는 형상 발생

--- online_lda.py
+++ online_lda.py
@@ -35,7 +35,7 @@ class LDA_VI:
         self.V = len(self.w2idx)
         self.D = len(self.data)
         self.Nd = [len(doc) for doc in self.data]
-        self.Ndw = [ [OrderedDict(sorted(Counter(doc).items()))]  for doc in self.doc2idx ]
+        self.Ndw = [ OrderedDict(sorted(Counter(doc).items()))  for doc in self.doc2idx ]
 
         # set initial value for free variational parameters
         self.phi = np.ones((self.V, self.K)) # dimension: for topic d, Nd * K
@@ -65,11 +65,11 @@ class LDA_VI:
     def _ELBO(self):
         term1 = 0 # E[ log p( w | phi, z) ]
         term2 = 0 # E[ log p( z | theta) ]
-        term3 = 0 # E[ log p( phi | beta) ]
+        term3 = 0 # E[ log q(z) ]
         term4 = 0 # E[ log p( theta | alpha) ]
-        term5 = 0 # E[ log q( z | psi* ) ]
-        term6 = 0 # E[ log q( phi | beta* ) ]
-        term7 = 0 # E[ log q( theta | alpha* ) ]
+        term5 = 0 # E[ log q( theta ) ]
+        term6 = 0 # E[ log p(beta | eta) ]
+        term7 = 0 # E[ log q(beta) ]
 
         '''
         ELBO is calculated w.r.t. each document
@@ -86,12 +86,12 @@ class LDA_VI:
                 self.lam_E[:,k] = self._E_dir(self.lam[:,k])
 
 
-            ndw = np.array(self.Ndw[d].keys())
+            ndw = np.array(list(self.Ndw[d].keys()))
             for k in range(self.K):
                 # update term 1
                 tmp = self.lam_E[:,k] * self.phi[:,k]
                 ndw_vec = np.zeros(self.V) # V * 1
-                ndw_vec[ndw] += self.Ndw[d].values()
+                ndw_vec[ndw] += np.array(list(self.Ndw[d].values()))
                 term1 += (tmp * ndw_vec).sum()
 
                 # update term 2
@@ -101,57 +101,28 @@ class LDA_VI:
 
                 # update term 3
                 tmp = self.phi[:,k] * log(self.phi[:,k] + 0.000000001)
-                term3 += (tmp * ndw_vec).sum
+                term3 += (tmp * ndw_vec).sum()
 
 
+            # update term 4
+            term4 += digamma(self.K * self.alpha) - log(self.K * gamma(self.alpha))
+            term4 += (self.alpha - 1) * self.gam_E[d,:].sum()
 
-            # update term2
-            # update dirichlet expectation of alpha_star
-            self.alpha_star_E[d, :] = self._E_dir(self.alpha_star[d, :])
-            tmp2 = self.alpha_star_E[d, :] # T dimensional
-            term2 += (self.psi_star[d] * tmp2[None,:]).sum()
+            # update term 5
+            term5 += digamma(sum(self.gam[d,:])) - sum(digamma(self.gam[d,:]))
+            term5 += ( (self.gam[d,:]-1) * self.gam_E[d,:] ).sum()
 
-            # update term5
-            # for numerical stability,add delta.
-            term5 += (self.psi_star[d] * log(self.psi_star[d] + 0.00000000001)).sum()
 
-        print('Done term 1,2,5')
-        print(f'term1: {term1}, term2: {term2}, term5: {term5}')
+        for k in range(self.K):
+            # update term 6
+            term6 += digamma(self.V * self.eta) - log(self.V * gamma(self.eta))
+            term6 += ( (self.eta-1) * self.lam[:,k] ).sum()
 
-        # update term3, 6
-        for t in range(self.T):
-            # update term3
-            term3 += self.beta_star_E[:, t].sum()
+            # update term 7
+            term7 += digamma(sum( self.lam[:,k] )) - sum( digamma(self.lam[:,k]) )
+            term7 += ( ( self.lam[:,k]-1 ) * ( self.lam_E[:,k] ) ).sum()
 
-            # update term6
-            logB = self._logB(self.beta_star[:, t]).sum()  # log ( dirichlet multiplicative factor)
-
-            # to prevent float error
-            if np.isnan(logB):
-                logB = 0
-            val = (self.beta_star[:,t] - 1) * self.beta_star_E[:, t]
-            term6 += val.sum() + logB
-        term3 *= self.beta - 1
-        print('Done term 3,6')
-        print(f'term3: {term3}, term6: {term6}')
-
-        # update term 4, 7
-        for d in range(self.D):
-            # update term4
-            term4 += self.alpha_star_E[d,:].sum()
-
-            # update term7
-            logB = self._logB(self.alpha_star[d,:]).sum()  # log ( dirichlet multiplicative factor)
-            if np.isnan(logB):
-                logB = 0
-            val = (self.alpha_star[d,:]-1) * self.alpha_star_E[d,:]
-            term7 += val.sum() + logB
-        term4 *= self.alpha - 1
-
-        print('Done term 4, 7')
-        print(f'term4: {term4}, term7: {term7}')
-
-        return term1 + term2 + term3 + term4 - term5 - term6 - term7
+        return term1 + term2 - term3 + term4 - term5 + term6 - term7
 
     def _E_dir(self, params):
         '''
@@ -178,7 +149,7 @@ class LDA_VI:
         for k in range(self.K):
             E_beta = self.lam_E[:,k][Nd_index] # Nd * 1: indexing for words in dth document
             E_theta = self.gam_E[d,:][k] # scalar: indexing for kth topic
-            self.phi[Nd_index:,k] = E_beta + E_theta # Nd * 1
+            self.phi[Nd_index,k] = E_beta + E_theta # Nd * 1
         ## vectorize to reduce time
         # to prevent overfloat
         self.phi -= self.phi.min(axis=1)[:,None]
@@ -191,17 +162,17 @@ class LDA_VI:
         tmp = self.Ndw[d]
         gam_dk = np.repeat(self.alpha, self.K)
         for w in self.doc2idx[d]:
-            gam_dk += tmp[w] * self.phi[w,:] # K * 1 dimension
+            gam_dk = gam_dk +  tmp[w] * self.phi[w,:] # K * 1 dimension
         self.gam[d,:] = gam_dk
 
 
     def _update_lam(self):
         for k in range(self.K):
-            lam_kw = self.eta
+            lam_k = np.repeat(self.eta, self.V)
             for d, doc in enumerate(self.doc2idx):
                 for w in doc:
-                    lam_kw += self.Ndw[d][w] * self.phi[w,:k]
-            self.lam[w, k] = lam_kw
+                    lam_kw = self.Ndw[d][w] * self.phi[w,k]
+                    lam_k[w] += lam_kw
             print(f'{k} topic finished')
 
 
@@ -227,8 +198,8 @@ class LDA_VI:
         print('Initializing Parms...')
         self._init_params()
         print(f'# of Documents: {self.D}')
-        print(f'# of unique vocabs: {self.M}')
-        print(f'{self.T} topics chosen')
+        print(f'# of unique vocabs: {self.V}')
+        print(f'{self.K} topics chosen')
 
         print('Start optimizing!')
         # initialize ELBO
@@ -238,7 +209,6 @@ class LDA_VI:
         self._ELBO_history.append(ELBO_before)
 
 
-        max_iter = 0
         print('##################### start training #####################')
         while abs(ELBO_after - ELBO_before) > threshold:
             start = time.time()
@@ -248,17 +218,17 @@ class LDA_VI:
             print('E step: start optimizing alpha_star...')
             # update psi_star, alpha_star
             for d in range(self.D):
-                alpha_star_before = self.alpha_star[d,:]
-                alpha_star_after = np.repeat(999,self.T)
-                while sum(abs(alpha_star_before - alpha_star_after)) / self.T > threshold:
-                    alpha_star_before = alpha_star_after
-                    self._update_Z_dn(d)
-                    self._update_theta_d(d)
-                    alpha_star_after = self.alpha_star[d,:]
+                gam_before = self.gam[d,:]
+                gam_after = np.repeat(999,self.K)
+                while sum(abs(gam_before - gam_after)) / self.K > threshold:
+                    gam_before = gam_after
+                    self._update_phi(d)
+                    self._update_gam(d)
+                    gam_after = self.gam[d,:]
 
             # update beta_star
-            print('M step: find phi_t that maximizes ELBO')
-            self._update_phi_t()
+            print('M step: Updating Beta..')
+            self._update_lam()
             print('Finished Inference!')
             print('\n')
 
--- practice.py
+++ practice.py
@@ -6,25 +6,13 @@ import pickle
 import time
 from online_lda import LDA_VI
 
-# dir = "/Users/shinbo/Desktop/metting/LDA/0. data/20news-bydate/newsgroup_preprocessed.pickle"
-# lda = LDA_VI(dir, 5, 0.1, 10)
-# lda._make_vocab()
-# lda._init_params()
-# print(lda.Ndw[0])
-#
-
-a = np.ones(5)
-b = np.array([1,1,1,1])
-print(a[b])
-print(a)
-
-
-#
-# lda.train(0.00001)
-# print(lda._ELBO_history)
-# pd.DataFrame(lda.psi_star[0]).to_csv('psi_star.csv', index=False)
-# pd.DataFrame(lda.alpha_star).to_csv('alpha_star.csv', index=False)
-# pd.DataFrame(lda.beta_star).to_csv('beta_star.csv', index=False)
+dir = "/Users/shinbo/Desktop/metting/LDA/0. data/20news-bydate/newsgroup_preprocessed.pickle"
+lda = LDA_VI(dir, 5, 0.1, 10)
+lda.train(0.00001)
+print(lda._ELBO_history)
+pd.DataFrame(lda.phi).to_csv('phi.csv', index=False)
+pd.DataFrame(lda.lam).to_csv('lam.csv', index=False)
+pd.DataFrame(lda.gam).to_csv('gam.csv', index=False)
 
 # from collections import Counter, OrderedDict
 # a = [1,2,3,3,3,1,1,2,2]
