496f803b441a2685706b573dd78895e938c3b854
shinbo <shinbo0613@gmail.com>
2021-01-30 01:18:50 +0900

    LDA upgrade 버전 코딩은 완료했는데 kappa 수렴을 손봐야함

---
+++ LDA/test_working_clda_ver2.py
@@ -0,0 +1,26 @@
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from WORKING_clda_ver2 import LDA_VI
+# #run my LDA model
+dir_ = "/Users/shinbo/Desktop/metting/LDA/0. data/20news-bydate/newsgroup_preprocessed_corpus.pickle"
+# dir_ = 'preprocessed_review.pickle'
+
+# for ordinary lda
+alpha = 5
+eta = 0.1
+K = 10
+maxIter = 1000
+maxIterDoc =100
+threshold = 10
+random_state = 42
+
+data = pickle.load(open(dir_, 'rb'))
+data_join = [' '.join(doc) for doc in data]
+cv = CountVectorizer()
+X = cv.fit_transform(data_join).toarray()
+
+lda = LDA_VI(alpha=alpha,eta=eta,K=K)
+lda.train(X, cv, maxIter, maxIterDoc, threshold, random_state)
+
+save_dir = '../../model_lda/LDA_PAPER_result.pkl'
+pickle.dump(lda, open(save_dir, 'wb'))
